b25d4d5a054d30cb31fe48dcc7a61f0d6ea61490
Ioannis Tsiftzis <ioannis.tsiftzis@camelotglobal.com>
2020-04-08 13:32:10 +0300

    next task solutions

---
+++ Problems/Board game basics  Game/src/Task.kt
@@ -0,0 +1,11 @@
+object PlayingField {
+    object Size {
+        var width = 0
+        var height = 0
+
+        fun changeSize(width: Int, height: Int) {
+            this.width = if (width < 0) 0 else width
+            this.height = if (height < 0) 0 else height
+        }
+    }
+}
